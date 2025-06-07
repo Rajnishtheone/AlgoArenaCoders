2c5281b8bcd4625999c4f4a43ef3a16539a7278f
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-06-07 21:07:53 +0530

    initial stage of my project

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.listen(process.env.PORT,()=>{
+    console.log(`process is running on the port${process.env.PORT}`)
+})
+
+
+
